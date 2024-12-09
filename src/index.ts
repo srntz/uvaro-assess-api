3b4a2d392ae55682f274c2444be1abf22cb01ab7
-srntz <tuzsocial909@gmail.com>
2024-12-08 17:28:22 -0700

    Enable cors for all origins and change server port to 4000

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ import {ApolloServer} from "@apollo/server";
 import {expressMiddleware} from "@apollo/server/express4";
 import express from "express";
 import {resolvers, typeDefs} from "./graphql";
+import cors from 'cors'
 
 dotenv.config();
 
@@ -11,9 +12,10 @@ const app = express();
 const server = new ApolloServer({typeDefs, resolvers});
 await server.start();
 
+app.use(cors())
 app.use(express.json());
 app.use("/graphql", expressMiddleware(server))
 
-app.listen(3000, () => {
-  console.log("Server started on port 3000");
+app.listen(4000, () => {
+  console.log("Server started on port 4000");
 })
