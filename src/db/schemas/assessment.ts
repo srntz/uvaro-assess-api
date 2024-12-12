36aac7eac16f3aa597b7ab442a0b0b359dfe386e
-srntz <tuzsocial909@gmail.com>
2024-12-11 23:24:50 -0700

    Update drizzle schemas to support user_id as uuid

--- src/db/schemas/assessment.ts
+++ src/db/schemas/assessment.ts
@@ -1,10 +1,10 @@
-import {integer, pgTable, serial, text, timestamp} from "drizzle-orm/pg-core";
+import {integer, pgTable, serial, text, timestamp, uuid} from "drizzle-orm/pg-core";
 import {user} from "./user";
 
 export const assessment = pgTable("assessment", {
   assessment_id: serial().primaryKey(),
   start_date_time: timestamp().notNull(),
-  end_date_time: timestamp().notNull(),
+  end_date_time: timestamp(),
   notes: text().notNull(),
-  user_id: integer().notNull().references(() => user.user_id)
+  user_id: uuid().notNull().references(() => user.user_id)
 })
--- src/db/schemas/user.ts
+++ src/db/schemas/user.ts
@@ -1,7 +1,7 @@
-import {pgTable, serial, varchar} from "drizzle-orm/pg-core";
+import {pgTable, serial, varchar, uuid} from "drizzle-orm/pg-core";
 
 export const user = pgTable("user", {
-  user_id: serial().primaryKey(),
+  user_id: uuid().primaryKey().defaultRandom(),
   first_name: varchar({length: 50}),
   last_name: varchar({length: 100}),
   email: varchar({length: 255}),
