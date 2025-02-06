5342cb6eaf753311ceb211a7a5b06de04ef04aaf
-srntz <tuzsocial909@gmail.com>
2025-02-06 11:48:30 -0700

    Add note service and mutation field for saving notes

--- src/graphql/entities/mutation.graphql.ts
+++ src/graphql/entities/mutation.graphql.ts
@@ -2,5 +2,6 @@ export const mutationEntity = `#graphql
     type Mutation {
         addUser(user: UserArgumentType!): User!
         addAssessment(user_id: String!): Assessment!
+        saveNote(assessment_id: Int!, category_id: Int!, note_text: String!): Note!
     }
-`
+`;
