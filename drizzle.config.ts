fba5eed08218886f8d2c6e45144a8dbbc5c2c97f
-srntz <tuzsocial909@gmail.com>
2025-02-11 12:04:53 -0700

    Add composite foreign key to assessment_answer. Add migration file.

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,6 +1,12 @@
 import { defineConfig } from "drizzle-kit";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 export default defineConfig({
   dialect: "postgresql",
   schema: "./src/db/schemas",
+  dbCredentials: {
+    url: process.env.UVARO_POSTGRES_URL,
+  },
 });
