cec087bee8493368c3b1f88456c8e1c754ba02f6
-srntz <tuzsocial909@gmail.com>
2025-04-09 15:46:55 -0600

    Change pg pool import to be ESM-compatible

--- src/db/DatabaseConnection.ts
+++ src/db/DatabaseConnection.ts
@@ -1,16 +1,16 @@
 import { drizzle } from "drizzle-orm/node-postgres";
 import * as dbSchema from "./schemas/index";
-import { Pool } from "pg";
+import pg from "pg";
 
 export class DatabaseConnection {
   private static instance: ReturnType<typeof drizzle>;
-  private static pool: Pool;
+  private static pool: pg.Pool;
 
   private constructor() {}
 
   public static getInstance() {
     if (!this.instance) {
-      this.pool = new Pool({
+      this.pool = new pg.Pool({
         connectionString: process.env.DATABASE_URL,
       });
       this.instance = drizzle(this.pool, {
